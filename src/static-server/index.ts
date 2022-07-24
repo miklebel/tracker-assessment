e7459854e3f8496705da8f843e317fc47ed404c1
Mikhail Beliakov <miklebel97@gmail.com>
2022-07-25 02:04:53 +0300

    remove express.static

--- src/static-server/index.ts
+++ src/static-server/index.ts
@@ -3,7 +3,6 @@ import express from "express";
 const main = () => {
   const app = express();
   const port = process.env.PORT || 8000;
-  app.use(express.static(__dirname + "/public"));
 
   app.get("*", (req, res) => {
     res.sendFile(__dirname + "/static/page.html");
